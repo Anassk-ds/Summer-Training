f6ec8e48cbfb5e0669ba625a9186f434bce24687
Anas Shaik <anasshaik382@gmail.com>
2026-07-05 21:13:26 +0530

    Fix count comparison indentation in homework.py
    Fixed indentation and formatting issues in the count comparison.

--- assignment8/homework.py
+++ assignment8/homework.py
@@ -1,7 +1,6 @@
 #Problem 1: Alternate Index Sum
 n = int(input())
 arr = list(map(int, input().split()))
-
 even_sum = 0
 odd_sum = 0
 for i in range(n):    
@@ -10,17 +9,15 @@ for i in range(n):
     else:        
         odd_sum += arr[i]
 print(even_sum, odd_sum)
-
 #Problem 2: Tuple Boundary Product
 n = int(input())
 t = tuple(map(int, input().split()))
 print(t[0] * t[-1])
-
 #Problem 3: Count Smaller Than Key
 n = int(input())
 arr = list(map(int, input().split()))
 k = int(input())
 count = 0
 for i in arr:    
-    if i < k:        count += 1
+    if i < k:count += 1
 print(count)
